c2464044b25a155e35285584559a7853b7480aae
KasparekKuba <kaspar37@student.vspj.cz>
2025-09-25 17:37:05 +0200

    Kartoteka aut

--- cviko_2/cviko_2/auta.c
+++ cviko_2/cviko_2/auta.c
@@ -1,15 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "auta.h"
 
-
-
-void pridej_auto(KARTOTEKA* karoteka, AUTO *auticko)
+void pridej_auto(KARTOTEKA* kartoteka)
 {
+    AUTO nove;
+    printf("Zadej znacku: ");
+    scanf("%31s", nove.znacka);
+    printf("Zadej typ: ");
+    scanf("%31s", nove.typ);
+    printf("Rok vyroby: ");
+    scanf("%d", &nove.rok_vyroby);
+    printf("Objem motoru: ");
+    scanf("%f", &nove.motor.objem);
+    printf("Pocet valcu: ");
+    scanf("%d", &nove.motor.pocet_valcu);
+    printf("Pocet ventilu: ");
+    scanf("%d", &nove.motor.pocet_ventilu);
+    printf("Pocet km: ");
+    scanf("%d", &nove.pocet_km);
+    printf("Majitel: ");
+    scanf("%31s", nove.majitel);
 
+    kartoteka->data = realloc(kartoteka->data, (kartoteka->head.pocet_aut + 1) * sizeof(AUTO));
+    kartoteka->data[kartoteka->head.pocet_aut] = nove;
+    kartoteka->head.pocet_aut++;
 
+    printf("Auto bylo pridano!\n");
 }
 
-
-void smaz_auto(AUTO auticko, int * index)
+void smaz_auto(KARTOTEKA* kartoteka, int index)
 {
+    if (index < 0 || index >= kartoteka->head.pocet_aut) {
+        printf("Chyba: index mimo rozsah!\n");
+        return;
+    }
 
-}kolokolomlynksé12
+    for (int i = index; i < kartoteka->head.pocet_aut - 1; i++) {
+        kartoteka->data[i] = kartoteka->data[i + 1];
+    }
+
+    kartoteka->head.pocet_aut--;
+    kartoteka->data = realloc(kartoteka->data, kartoteka->head.pocet_aut * sizeof(AUTO));
+
+    printf("Auto na indexu %d bylo smazano.\n", index);
+}
+
+void vypis_aut(const KARTOTEKA* kartoteka)
+{
+    if (kartoteka->head.pocet_aut == 0) {
+        printf("Kartoteka je prazdna.\n");
+        return;
+    }
+
+    for (int i = 0; i < kartoteka->head.pocet_aut; i++) {
+        AUTO a = kartoteka->data[i];
+        printf("%d) %s %s, rok %d, motor %.1fL (%dV/%dV), %d km, majitel: %s\n",
+            i, a.znacka, a.typ, a.rok_vyroby,
+            a.motor.objem, a.motor.pocet_valcu, a.motor.pocet_ventilu,
+            a.pocet_km, a.majitel);
+    }
+}
--- cviko_2/cviko_2/main.c
+++ cviko_2/cviko_2/main.c
@@ -1,15 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "auta.h"
-#include <stdio.h>
-#include <stdlib.h>
 
+int main() {
+    KARTOTEKA kartoteka;
+    kartoteka.head.verze = 1.0f;
+    kartoteka.head.pocet_aut = 0;
+    kartoteka.data = NULL;
 
+    int volba;
+    do {
+        printf("\n--- MENU ---\n");
+        printf("1 - Pridat auto\n");
+        printf("2 - Smazat auto\n");
+        printf("3 - Vypis vsech aut\n");
+        printf("0 - Konec\n");
+        printf("Zadej volbu: ");
+        scanf("%d", &volba);
+        while (getchar() != '\n'); // vyèistí zbytek vstupu
 
-void main()
-{
+        switch (volba) {
+        case 1:
+            pridej_auto(&kartoteka);
+            break;
+        case 2: {
+            int index;
+            printf("Zadej index auta ke smazani: ");
+            scanf("%d", &index);
+            while (getchar() != '\n'); // vyèistí zbytek vstupu
+            smaz_auto(&kartoteka, index);
+            break;
+        }
+        case 3:
+            vypis_aut(&kartoteka);
+            break;
+        case 0:
+            printf("Konec programu.\n");
+            break;
+        default:
+            printf("Neplatna volba!\n");
+        }
+    } while (volba != 0);
 
-
-
-
+    free(kartoteka.data);
+    return 0;
 }
-
 
